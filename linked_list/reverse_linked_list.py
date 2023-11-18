989533ce706ecb5ddf308ff32b614387ffd0ddad
emmadev <macanthonyemmanuel12@gmail.com>
2023-11-18 09:04:31 +0100

    reverse linked list

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,23 @@
+"""Given the head of a singly linked list, reverse the list, and return the reversed list."""
+
+
+class ListNode(object):
+    
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+        
+def reverse_linked_list(head: ListNode):
+    
+    prev = None
+    current = head
+    
+    while current:
+        temp = current.next
+        current.next = prev
+        prev = current
+        current = temp
+        
+    return prev
+        
+    
