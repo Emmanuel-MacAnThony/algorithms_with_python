56af054883922766571c2dd2d88d94af71030504
emmadev <macanthonyemmanuel12@gmail.com>
2023-11-26 14:40:08 +0100

    solvd is subtree of another tree

---
+++ trees/is_subtree_of_another_tree.py
@@ -0,0 +1,29 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+"""
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+"""
+def isSubtree(root: TreeNode, subRoot: TreeNode):
+    if not root:
+            return False
+
+    if sameTree(root, subRoot):
+            return True
+
+    return isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot)
+
+
+def sameTree(s:TreeNode,t: TreeNode):
+        if not s and not t:
+            return True
+
+        if s and t and s.val == t.val:
+            return sameTree(s.left, t.left) and sameTree(s.right, t.right)
+
+        return False
